2618287c316decac74de6ca9605459737c3882e4
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-11 11:48:06 +0200

    Update scripts

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,17 +1,19 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultV2');
 
-  const wantAddress = '0x049d68029688eAbF473097a2fC38ef61633A3C7A';
-  const tokenName = 'fUSDT Crypt';
-  const tokenSymbol = 'rfUSDT';
-  const tvlCap = ethers.utils.parseEther('1000');
+  const wantAddress = '0x4200000000000000000000000000000000000006';
+  const tokenName = 'ETH Crypt';
+  const tokenSymbol = 'rfETH';
+  const tvlCap = ethers.utils.parseEther('1');
 
   const strategist1 = '0x1E71AEE6081f62053123140aacC7a06021D77348';
   const strategist2 = '0x81876677843D00a7D792E1617459aC2E93202576';
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
-  const superAdmin = '0x04C710a1E8a738CDf7cAD3a52Ba77A784C35d8CE';
-  const admin = '0x539eF36C804e4D735d8cAb69e8e441c12d4B88E0';
-  const guardian = '0xf20E25f2AB644C8ecBFc992a6829478a85A98F2c';
+
+  const superAdmin = '0x9BC776dBb134Ef9D7014dB1823Cd755Ac5015203';
+  const admin = '0xeb9C9b785aA7818B2EBC8f9842926c4B9f707e4B';
+  const guardian = '0xb0C9D5851deF8A2Aac4A23031CA2610f8C3483F9';
+
   const strategists = [strategist1, strategist2, strategist3];
   const multisigRoles = [superAdmin, admin, guardian];
 
--- scripts/deploy/2-strategy.js
+++ scripts/deploy/2-strategy.js
@@ -1,19 +1,26 @@
 const hre = require('hardhat');
 
 async function main() {
-  const vaultAddress = '0xa9A9dB466685F977F9ECEe347958bcef90498177';
+  const vaultAddress = '';
 
   const Strategy = await ethers.getContractFactory('ReaperStrategyGeist');
-  const treasuryAddress = '0x0e7c5313E9BB80b654734d9b7aB1FB01468deE3b';
-  const paymentSplitterAddress = '0x63cbd4134c2253041F370472c130e92daE4Ff174';
+
+  const treasuryAddress = '0xeb9C9b785aA7818B2EBC8f9842926c4B9f707e4B';
+  const paymentSplitterAddress = '0x2b394b228908fb7DAcafF5F340f1b442a39B056C';
+
   const strategist1 = '0x1E71AEE6081f62053123140aacC7a06021D77348';
   const strategist2 = '0x81876677843D00a7D792E1617459aC2E93202576';
   const strategist3 = '0x1A20D7A31e5B3Bc5f02c8A146EF6f394502a10c4';
-  const superAdmin = '0x04C710a1E8a738CDf7cAD3a52Ba77A784C35d8CE';
-  const admin = '0x539eF36C804e4D735d8cAb69e8e441c12d4B88E0';
-  const guardian = '0xf20E25f2AB644C8ecBFc992a6829478a85A98F2c';
-  const gWant = '0x940f41f0ec9ba1a34cf001cc03347ac092f5f6b5';
-  const targetLtv = 7800;
+
+  const superAdmin = '0x9BC776dBb134Ef9D7014dB1823Cd755Ac5015203';
+  const admin = '0xeb9C9b785aA7818B2EBC8f9842926c4B9f707e4B';
+  const guardian = '0xb0C9D5851deF8A2Aac4A23031CA2610f8C3483F9';
+
+  const usdcAddress = '0x7F5c764cBc14f9669B88837ca1490cCa17c31607';
+  const wantAddress = '0x4200000000000000000000000000000000000006';
+  const wantToUsdcPath = [wantAddress, usdcAddress];
+  const wantToUsdcFee = [500];
+  const poolIndex = 5;
 
   const strategy = await hre.upgrades.deployProxy(
     Strategy,
@@ -22,9 +29,9 @@ async function main() {
       [treasuryAddress, paymentSplitterAddress],
       [strategist1, strategist2, strategist3],
       [superAdmin, admin, guardian],
-      gWant,
-      targetLtv,
-      targetLtv + 40,
+      wantToUsdcPath,
+      wantToUsdcFee,
+      poolIndex,
     ],
     {kind: 'uups', timeout: 0},
   );
