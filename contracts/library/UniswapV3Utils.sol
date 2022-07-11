790da83b9d1ee24595f327ee5604d889195fa80a
tess3rac7 <tess3rac7@protonmail.com>
2022-07-11 11:26:25 -0400

    Include univ3 imports locally.

--- contracts/library/UniswapV3Utils.sol
+++ contracts/library/UniswapV3Utils.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '../interfaces/ISwapRouter.sol';
 // import "@uniswap/v3-periphery/contracts/libraries/Path.sol";
 
 library UniswapV3Utils {
