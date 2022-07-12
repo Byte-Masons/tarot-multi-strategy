4abd3e00a60438584d72980262e039c077f88079
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-12 14:02:15 +0200

    Modify approve and keeper scripts

--- scripts/approve-vault.js
+++ scripts/approve-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const vaultAddress = '0x17D099fc623bd06CFE4861d874704Af184773c75';
   const ERC20 = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20');
-  const wantAddress = '';
+  const wantAddress = '0x4200000000000000000000000000000000000006';
   const want = await ERC20.attach(wantAddress);
-  await want.approve(vaultAddress, ethers.utils.parseEther('1000'));
+  await want.approve(vaultAddress, ethers.utils.parseEther('1'));
   console.log('want approved');
 }
 
